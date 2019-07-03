5fdb7d86cf2a1f93717fda4aa351227ec6c4e5fc
Yuhao Zhou <miskcoo@gmail.com>
2019-07-03 23:44:34 +0800

    Update I$/CPU interface.

--- src/common_defs.svh
+++ src/common_defs.svh
@@ -19,18 +19,25 @@ typedef uint32_t      virt_t;
 typedef uint32_t      phys_t;
 
 // interface of I$ and CPU
+// I$ is 2-stage pipelined
 interface cpu_ibus_if();
 	logic read, stall;
-	phys_t address;   // aligned in 8bytes
+	phys_t address;   // aligned in 8-bytes
 	uint64_t rddata;
+
+	// indicate that corresponding stage can be directly terminated.
+	// flush_1 will be '1' whenever flush_2 is '1'.
+	logic flush_1, flush_2;
 
     modport master (
 		output read, address,
+		output flush_1, flush_2,
 		input  stall, rddata
     );
 
     modport slave (
 		input  read, address,
+		input  flush_1, flush_2,
 		output stall, rddata
     );
 
@@ -42,7 +49,7 @@ interface cpu_dbus_if();
 	logic read, write, stall;
 	logic uncached_read, uncached_write, uncached_stall;
 	logic [3:0] byteenable;
-	phys_t address;      // aligned in 4bytes
+	phys_t address;      // aligned in 4-bytes
 	uint32_t rddata, wrdata, uncached_rddata;
 
     modport master (
