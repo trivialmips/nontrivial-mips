7f12fe33c9c149554f90477a706e844bb2c2e524
Harry Chen <i@harrychen.xyz>
2019-07-07 01:18:47 +0800

    Add SoC top design and IOBUF helper header
    Signed-off-by: Harry Chen <i@harrychen.xyz>

---
+++ src/utils/iobuf_helper.svh
@@ -0,0 +1,24 @@
+`define IOBUF_GEN(IN, OUT) wire OUT``_i, OUT``_o, OUT``_t; \
+IOBUF IN``_buf ( \
+    .IO(IN), \
+    .I(OUT``_i), \
+    .O(OUT``_o), \
+    .T(OUT``_t) \
+);
+
+`define IOBUF_GEN_SIMPLE(IN) `IOBUF_GEN(IN, IN)
+
+`define IOBUF_GEN_VEC(IN, OUT) wire [$bits(IN) - 1:0] OUT``_i, OUT``_o, OUT``_t; \
+genvar IN``_gen_var; \
+generate \
+  for(IN``_gen_var = 0; IN``_gen_var < $bits(IN); IN``_gen_var = IN``_gen_var + 1) begin: IN``_buf_gen \
+    IOBUF IN``_buf ( \
+      .IO(IN[IN``_gen_var]), \
+      .O(OUT``_o[IN``_gen_var]), \
+      .I(OUT``_i[IN``_gen_var]), \
+      .T(OUT``_t[IN``_gen_var]) \
+    ); \
+  end \
+endgenerate
+
+`define IOBUF_GEN_VEC_SIMPLE(IN) `IOBUF_GEN_VEC(IN, IN)
