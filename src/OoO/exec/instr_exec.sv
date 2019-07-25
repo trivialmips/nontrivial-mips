6d2c53844581d4e624a43beeaaa284785e9dffa8
Yuhao Zhou <miskcoo@gmail.com>
2019-07-26 07:25:55 +0800

    Add ALU module.

--- src/OoO/exec/instr_exec.sv
+++ src/OoO/exec/instr_exec.sv
@@ -3,6 +3,7 @@
 module instr_exec(
 	input  logic             clk,
 	input  logic             rst,
+	input  logic             flush,
 
 	input  logic             [1:0] alu_taken,
 	output logic             [1:0] alu_ready,
@@ -12,7 +13,30 @@ module instr_exec(
 	output reserve_station_t [1:0] rs_i,
 
 	// CDB
-	input  cdb_packet_t      cdb_i
+	output cdb_packet_t      cdb_o
 );
+
+// CDB
+cdb_packet_t cdb;
+
+// ALU information
+uint32_t  [`ALU_RS_SIZE-1:0] alu_data;
+logic     [`ALU_RS_SIZE-1:0] alu_data_ready;
+logic     [`ALU_FU_SIZE-1:0] alu_data_ack;
+
+alu_rs alu_rs_inst(
+	.clk,
+	.rst,
+	.flush,
+	.rs_taken ( alu_taken ),
+	.rs_ready ( alu_ready ),
+	.rs_index ( alu_index ),
+	.rs_i,
+	.data       ( alu_data       ),
+	.data_ready ( alu_data_ready ),
+	.data_ack   ( alu_data_ack   ),
+	.cdb
+);
+
 
 endmodule
