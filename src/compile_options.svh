25f1bdf81a85f25f60bcbe39601047fe5a2d6dec
Harry Chen <i@harrychen.xyz>
2019-07-27 20:36:26 +0800

    Add COMPILE_FULL flag in compilation options
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/compile_options.svh
+++ src/compile_options.svh
@@ -7,9 +7,11 @@
 
 **/
 
-`define CPU_MMU_ENABLED      1
-`define CPU_LLSC_ENABLED     1
-`define CPU_MUTEX_PRIV       1
+`define COMPILE_FULL 1
+
+`define CPU_MMU_ENABLED      `COMPILE_FULL
+`define CPU_LLSC_ENABLED     `COMPILE_FULL
+`define CPU_MUTEX_PRIV       `COMPILE_FULL
 
 `define FETCH_NUM            2
 `define ISSUE_NUM            2
