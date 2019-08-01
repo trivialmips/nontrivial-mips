a7a1f90768bb5e325f06e801905e375fc5572db3
Yuhao Zhou <miskcoo@gmail.com>
2019-08-01 11:10:56 +0800

    Pipeline uncached access.

--- src/cache/dcache_pass.sv
+++ src/cache/dcache_pass.sv
@@ -42,15 +42,17 @@ logic pipe_read;
 logic pipe_write;
 logic [3:0] pipe_byteenable;
 logic [31:0] pipe_addr;
-uint32_t pipe_wdata;
+uint32_t pipe_wdata, pipe_rdata;
 
 // AXI Plumbing
 assign axi_req_arid = '0;
 assign axi_req_awid = '0;
 assign axi_req_wid = '0;
 
-assign dbus.stall  = (state_d != IDLE && state_d != WAIT_BVALID);
-assign dbus.rddata = axi_resp.rdata;
+logic pipe_stall, stall;
+assign stall = (state_d != IDLE && state_d != WAIT_BVALID);
+assign dbus.stall = pipe_stall;
+assign dbus.rddata = pipe_rdata;
 
 logic uncache_access;
 assign uncache_access = (pipe_read | pipe_write) & (state == IDLE | state == WAIT_BVALID);
@@ -59,8 +61,8 @@ always_comb begin
 	state_d = state;
 	case(state)
 		IDLE: begin
-			if(pipe_read)  state_d = axi_resp.arready ? READ  : READ_WAIT_AXI;
-			if(pipe_write) state_d = axi_resp.awready ? WRITE : WRITE_WAIT_AXI;
+			if(pipe0_read)  state_d = axi_resp.arready ? READ  : READ_WAIT_AXI;
+			if(pipe0_write) state_d = axi_resp.awready ? WRITE : WRITE_WAIT_AXI;
 		end
 		READ_WAIT_AXI:  if(axi_resp.arready) state_d = READ;
 		WRITE_WAIT_AXI: if(axi_resp.awready) state_d = WRITE;
@@ -69,16 +71,16 @@ always_comb begin
 		WAIT_BVALID: begin
 			if(axi_resp.bvalid) begin
 				state_d = IDLE;
-				if(pipe_read)  state_d = READ_WAIT_AXI;
-				if(pipe_write) state_d = WRITE_WAIT_AXI;
-			end else if(pipe_read | pipe_write) begin
+				if(pipe0_read)  state_d = READ_WAIT_AXI;
+				if(pipe0_write) state_d = WRITE_WAIT_AXI;
+			end else if(pipe0_read | pipe0_write) begin
 				state_d = WAIT_BVALID_RW;
 			end
 		end
 		WAIT_BVALID_RW: begin
 			if(axi_resp.bvalid) begin
-				if(pipe_read)  state_d = READ_WAIT_AXI;
-				if(pipe_write) state_d = WRITE_WAIT_AXI;
+				if(pipe0_read)  state_d = READ_WAIT_AXI;
+				if(pipe0_write) state_d = WRITE_WAIT_AXI;
 			end
 		end
 	endcase
@@ -93,16 +95,16 @@ always_comb begin
 	axi_req.awlen   = 3'b0000;
 	axi_req.arsize  = 2'b010; // 4 bytes
 	axi_req.awsize  = 2'b010;
-	axi_req.wstrb   = pipe_byteenable;
-	axi_req.araddr  = pipe_addr;
-	axi_req.awaddr  = pipe_addr;
-	axi_req.wdata   = pipe_wdata;
+	axi_req.wstrb   = pipe0_byteenable;
+	axi_req.araddr  = pipe0_addr;
+	axi_req.awaddr  = pipe0_addr;
+	axi_req.wdata   = pipe0_wdata;
 	axi_req.bready = 1'b1;
 
 	case(state)
 		IDLE: begin
-			axi_req.arvalid = pipe_read;
-			axi_req.awvalid = pipe_write;
+			axi_req.arvalid = pipe0_read;
+			axi_req.awvalid = pipe0_write;
 		end
 		READ_WAIT_AXI:  axi_req.arvalid = 1'b1;
 		WRITE_WAIT_AXI: axi_req.awvalid = 1'b1;
@@ -128,20 +130,28 @@ always_ff @(posedge clk) begin
 		pipe0_read       <= 1'b0;
 		pipe0_write      <= 1'b0;
 		pipe0_byteenable <= '0;
-		pipe_addr       <= '0;
-		pipe_read       <= 1'b0;
-		pipe_write      <= 1'b0;
-		pipe_byteenable <= '0;
-	end else if(~dbus.stall) begin
+	end else if(~stall) begin
 		pipe0_read       <= dbus.read;
 		pipe0_write      <= dbus.write;
 		pipe0_addr       <= dbus.address;
 		pipe0_wdata      <= dbus.wrdata;
 		pipe0_byteenable <= dbus.byteenable;
+	end
+
+	if(rst) begin
+		pipe_addr       <= '0;
+		pipe_read       <= 1'b0;
+		pipe_write      <= 1'b0;
+		pipe_stall      <= 1'b0;
+		pipe_rdata      <= '0;
+		pipe_byteenable <= '0;
+	end else begin
 		pipe_read       <= pipe0_read;
 		pipe_write      <= pipe0_write;
 		pipe_addr       <= pipe0_addr;
 		pipe_wdata      <= pipe0_wdata;
+		pipe_stall      <= stall;
+		pipe_rdata      <= axi_resp.rdata;
 		pipe_byteenable <= pipe0_byteenable;
 	end
 end
